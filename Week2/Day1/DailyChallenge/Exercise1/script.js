7b4126160077dfc41ce97df0fedab7af898480fa
yonizs <yoni.shiller@gmail.com>
2022-03-21 01:16:20 +0200

    relevant code

---
+++ Week2/Day1/DailyChallenge/Exercise1/script.js
@@ -0,0 +1,7 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits.splice(0,1));
+fruits.sort;
+fruits.push('kiwi');
+fruits.shift();
+fruits.reverse();
+console.log(fruits);
