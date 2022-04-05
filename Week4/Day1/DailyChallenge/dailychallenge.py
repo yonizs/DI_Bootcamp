ade24c058caae828b4139c5bc3cf5b7d380a5664
yonizs <yoni.shiller@gmail.com>
2022-04-05 04:15:47 +0300

    comment

---
+++ Week4/Day1/DailyChallenge/dailychallenge.py
@@ -0,0 +1,20 @@
+string = int(input("Please enter a string"))
+num = len(string)
+if num < 10:
+ print("string not long enough")
+
+else: 
+  print("string not long enough")
+
+print("First character:", string)   
+print("Last character:", string)
+print(string[0])
+print(string[0,1])
+print(string[0,1,2])
+print(string[0,1,2,3])
+print(string[0,1,2,3,4])
+print(string[0,1,2,3,4,5])
+print(string[0,1,2,3,4,5,6])
+print(string[0,1,2,3,4,5,6,7])
+print(string[0,1,2,3,4,5,6,7,8])
+print(string[0,1,2,3,4,5,6,7,8,9])
