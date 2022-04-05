eff18cd0db43c47cf377acc5daf5059044ca85b5
yonizs <yoni.shiller@gmail.com>
2022-04-05 04:18:57 +0300

    comment

--- Week4/Day1/DailyChallenge/dailychallenge.py
+++ Week4/Day1/DailyChallenge/dailychallenge.py
@@ -18,3 +18,6 @@ print(string[0,1,2,3,4,5,6])
 print(string[0,1,2,3,4,5,6,7])
 print(string[0,1,2,3,4,5,6,7,8])
 print(string[0,1,2,3,4,5,6,7,8,9])
+import random
+random.shuffle(string)
+print(string)
